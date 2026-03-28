6c5da2f7e686ee5510012077a94806b6f30cea57
topa-mihai-sebastian <mihai.topa@stud.acs.upb.ro>
2026-03-28 12:20:28 +0200

    better code arrangement

--- Vector/vector.cpp
+++ Vector/vector.cpp
@@ -8,12 +8,16 @@ namespace sebi
 template <typename T>
 class vector
 {
-  private:
-    T *data;
-    size_t m_size;
-    size_t m_capacity;
-
   public:
+    // ==========================================
+    // 1. ALIAS-URI SI TIPURI
+    // ==========================================
+    using iterator = T *;
+    using const_iterator = const T *;
+
+    // ==========================================
+    // 2. CONSTRUCTORI & DESTRUCTOR
+    // ==========================================
     // constructor default
     vector()
     {
@@ -22,6 +26,7 @@ class vector
         m_size = 0;
         m_capacity = 0;
     }
+
     vector(size_t _capacity) : m_size(0), m_capacity(_capacity)
     {
         if (m_capacity > 0)
@@ -33,6 +38,7 @@ class vector
             data = nullptr;
         }
     }
+
     // copy constr
     vector(const vector<T> &other) : m_size(other.m_size), m_capacity(other.m_capacity)
     {
@@ -49,14 +55,16 @@ class vector
             data = nullptr;
         }
     }
+
+    // move constructor
     vector(vector<T> &&other) noexcept : data(other.data), m_size(other.m_size), m_capacity(other.m_capacity)
     {
         other.data = nullptr;
         other.m_size = 0;
         other.m_capacity = 0;
     }
-    // constructor pentru initializari de tipul
-    // vector<int> v ={1, 2, 3};
+
+    // constructor pentru initializari de tipul: vector<int> v = {1, 2, 3};
     vector(std::initializer_list<T> list) : m_size(list.size()), m_capacity(list.size())
     {
         if (m_capacity > 0)
@@ -73,16 +81,15 @@ class vector
             data = nullptr;
         }
     }
-    void push_back(const T &element)
+
+    ~vector()
     {
-        if (m_size == m_capacity)
-        {
-            std::cout << "index curent: " << m_size << ", capacitate curenta: " << m_capacity << std::endl;
-            std::cout << "se face o realocare de momorie" << std::endl;
-            reserve(m_capacity == 0 ? 1 : m_capacity * 2);
-        }
-        data[m_size++] = element;
+        delete[] data;
     }
+
+    // ==========================================
+    // 3. OPERATORI DE ATRIBUIRE (=)
+    // ==========================================
     // copy assign operator
     vector &operator=(const vector<T> &other)
     {
@@ -108,6 +115,7 @@ class vector
 
         return *this;
     }
+
     // move assign operator
     vector &operator=(vector<T> &&other) noexcept
     {
@@ -127,76 +135,74 @@ class vector
 
         return *this;
     }
-    vector operator+(const vector<T> &other) const
-    {
-        if (m_size != other.m_size)
-        {
-            throw std::invalid_argument("Vectorii nu au aceeași dimensiune!");
-        }
 
-        vector<T> result(*this); // Facem o copie a vectorului curent (folosind Copy Constructor-ul tău)
-        // te foloseste de operatorul += implementat mai sus
-        result += other;
-
-        return result;
-    }
-    vector &operator+=(const vector<T> &other)
-    {
-        if (m_size != other.m_size)
-        {
-            throw std::invalid_argument("Vectorii nu au aceeași dimensiune!");
-        }
-        for (size_t i = 0; i < m_size; i++)
-        {
-            data[i] += other.data[i];
-        }
-        return *this;
-    }
-    void pop_back()
-    {
-        // Ne asigurăm că avem ce șterge
-        if (m_size > 0)
-        {
-            m_size--;
-        }
-    }
+    // ==========================================
+    // 4. ACCES LA ELEMENTE
+    // ==========================================
     // pentru a modifica elementele : v[10] = 5;
     T &operator[](size_t index)
     {
         return data[index];
     }
+
     // std::cout<<v[5];
     const T &operator[](size_t index) const
     {
         return data[index];
     }
+
     T &back()
     {
-        // Este bine să gestionăm cazul în care vectorul e gol
+        // este bine sa gestionăm cazul in care vectorul e gol
         if (m_size == 0)
         {
             throw std::out_of_range("Vector is empty");
         }
         return data[m_size - 1];
     }
+
+    // ==========================================
+    // 5. ITERATORI
+    // ==========================================
+    iterator begin()
+    {
+        return data; // returnam pointerul la inceputul array-ului
+    }
+
+    iterator end()
+    {
+        return data + m_size; // pointer dupa ultimul element valabil (exclusiv)
+    }
+
+    // varianta constanta - read only
+    const_iterator begin() const
+    {
+        return data;
+    }
+
+    const_iterator end() const
+    {
+        return data + m_size;
+    }
+
+    // ==========================================
+    // 6. CAPACITATE ȘI DIMENSIUNE
+    // ==========================================
     size_t size() const
     {
         return m_size;
     }
+
     size_t capacity() const
     {
         return m_capacity;
     }
-    // acesta este un free logic
-    // nu necesita delete[]
-    void clear()
-    {
-        m_size = 0;
-    }
+
     bool empty() const
     {
         return m_size == 0;
     }
+
     void reserve(size_t new_cap)
     {
         // pentru a nu face vectorul sa fie mai mic
@@ -214,33 +220,70 @@ class vector
         }
     }
 
-    // iteratori
-    using iterator = T *;
-    using const_iterator = const T *;
-
-    iterator begin()
+    // ==========================================
+    // 7. MODIFICATORI
+    // ==========================================
+    void push_back(const T &element)
     {
-        return data; // Returnam pointerul la inceputul array-ului
-    }
-    iterator end()
-    {
-        return data + m_size; // Pointer dupa ultimul element valabil (exclusiv)
-    }
-    // varianta constanta
-    // read only
-    const_iterator begin() const
-    {
-        return data;
-    }
-    const_iterator end() const
-    {
-        return data + m_size;
+        if (m_size == m_capacity)
+        {
+            std::cout << "index curent: " << m_size << ", capacitate curenta: " << m_capacity << std::endl;
+            std::cout << "se face o realocare de momorie" << std::endl;
+            reserve(m_capacity == 0 ? 1 : m_capacity * 2);
+        }
+        data[m_size++] = element;
     }
 
-    ~vector()
+    void pop_back()
     {
-        delete[] data;
+        // ne asiguram ca avem ce sterge
+        if (m_size > 0)
+        {
+            m_size--;
+        }
     }
+
+    // acesta este un free logic
+    // nu necesita delete[]
+    void clear()
+    {
+        m_size = 0;
+    }
+
+    // ==========================================
+    // 8. OPERATORI EXTRA
+    // ==========================================
+    vector &operator+=(const vector<T> &other)
+    {
+        if (m_size != other.m_size)
+        {
+            throw std::invalid_argument("vectorii nu au aceeasi dimensiune!");
+        }
+        for (size_t i = 0; i < m_size; i++)
+        {
+            data[i] += other.data[i];
+        }
+        return *this;
+    }
+
+    vector operator+(const vector<T> &other) const
+    {
+        if (m_size != other.m_size)
+        {
+            throw std::invalid_argument("Vectorii nu au aceeași dimensiune!");
+        }
+        // facem o copie a vectorului curent (folosind copy constructor-ul)
+        vector<T> result(*this);
+        // se foloseste de operatorul += implementat mai sus
+        result += other;
+
+        return result;
+    }
+
+  private:
+    T *data;
+    size_t m_size;
+    size_t m_capacity;
 };
 } // namespace sebi
 int main()
